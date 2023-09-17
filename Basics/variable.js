3f46e0b86a6db45d4925ede1202c96a52872c9f6
maulik6307 <109839658+maulik6307@users.noreply.github.com>
2023-09-17 15:40:11 +0000

    variable add

---
+++ Basics/variable.js
@@ -0,0 +1,6 @@
+const accountId = 112121
+let accountEmail = "Maulik@gmail.com"
+var accountPassword = "12345"
+accountCity = "Botad"
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
