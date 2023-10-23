184220ba80b012d0a104330162534931996d7417
maulik6307 <109839658+maulik6307@users.noreply.github.com>
2023-10-23 11:29:16 +0000

    operation

---
+++ Basics/operation.js
@@ -0,0 +1,11 @@
+// console.log("1"+2) //=>12
+
+// console.log(1+"2") //=>12
+
+// console.log("1"+2+2) //=>122
+
+// console.log(1+2+"2") //=>32
+
+// console.log(Number(true)+1); //=>2
+
+console.log("1"+1+Number(false)); //=>110
